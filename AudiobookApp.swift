916d4008570489b636b3e0708ec859fa7f244cb8
Room-designer-013-15ysr <frommynotebook13@gmail.com>
2026-09-11 09:48:36 -0500

    Create AudiobookApp.swift

---
+++ AudiobookApp.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+import AVFoundation
+
+@main
+struct AudiobookApp: App {
+    init() {
+        configureAudioSession()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+    private func configureAudioSession() {
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(.playback, mode: .spokenAudio, options: [])
+            try session.setActive(true)
+        } catch {
+            print("Failed to set up AVAudioSession: \(error.localizedDescription)")
+        }
+    }
+}
